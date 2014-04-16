1c20cbdfa345113bc98e7beb2d349fce9be04faa
jatr <jatr@kb.dk>
2014-04-16 13:03:11 +0200

    Using the annotorious plugin API to add the desired drop-down menu

---
+++ js/tei-annotation.js
@@ -0,0 +1,15 @@
+/**
+ * Created by jatr on 4/14/14.
+ */
+annotorious.plugin.TeiAnnotationPlugin = function(opt_config_options) { }
+
+annotorious.plugin.TeiAnnotationPlugin.prototype.initPlugin = function(anno) {
+    // Add initialization code here, if needed (or just skip this method if not)
+};
+
+annotorious.plugin.TeiAnnotationPlugin.prototype.onInitAnnotator = function(annotator) {
+    // A Field can be an HTML string or a function(annotation) that returns a string
+    annotator.editor.addField(function(field) {
+        return '<select id="tei_field_type"><option id="tei_text">Text</option></select>'
+    });
+};
